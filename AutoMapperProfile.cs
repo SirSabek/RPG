e5983228fd7852f914019ca60d5a59e96ddc9efc
SirSabek <mmsabek99@gmail.com>
2023-04-22 13:24:02 +0200

    adding automapper profile and create first migration

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RPG.Dtos.Character;
+using RPG.Models;
+
+namespace RPG
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
