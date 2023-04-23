0ce2b401b16bec510bb26c771110c5e3bc076a0b
SirSabek <mmsabek99@gmail.com>
2023-04-23 20:38:31 +0200

    adding the weapon table

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,6 +10,7 @@ namespace RPG.Data
         }
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
 
     }
 }
