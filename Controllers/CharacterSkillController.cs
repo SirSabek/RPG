d1bfb4c28407b7e84250cab91db494731c9c8eb2
SirSabek <mmsabek99@gmail.com>
2023-04-25 15:46:34 +0200

    adding CharacterSkill Controller

---
+++ Controllers/CharacterSkillController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RPG.Dtos.CharacterSkill;
+using RPG.Services.CharacterSkillService;
+
+namespace RPG.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    [Authorize]
+    public class CharacterSkillController : ControllerBase
+    {
+        private readonly ICharacterSkillService _characterSkillService;
+
+        public CharacterSkillController(ICharacterSkillService characterSkillService)
+        {
+            _characterSkillService = characterSkillService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill)
+        {
+            return Ok(await _characterSkillService.AddCharacterSkill(newCharacterSkill));
+        }
+    }
+}
